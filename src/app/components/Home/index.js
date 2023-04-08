f1920f9acc04150b2311f79992bb8e07e6253549
Amir Pasha Bagheri <a.p.bagheri83@gmail.com>
2023-04-09 00:04:11 +0430

    update packages

---
+++ src/app/components/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Starter from './Starter'
+
+function Home(props) {
+  return (
+    <>
+      <Starter />
+    </>
+  )
+}
+
+Home.propTypes = {}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import Home from './app/components/Home'
+import './app/styles/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 )
